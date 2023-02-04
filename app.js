7902b9e7e781ea9ef91bc86b29d1049f97372e99
madhurimaDani <madhurimadani@gmail.com>
2023-02-04 14:34:24 -0800

    implementated bcrypt authentication (hashing and salting)

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+// const md5 = require("md5");
 // const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 
 const app = express();
 
@@ -39,18 +42,21 @@ app.route("/register")
 })
 
 .post(function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
 
-    newUser.save(function(err){
-        if(!err){
-            res.render("secrets");
-        }else{
-            res.status(500).send("Error while registering /n" + err);
-        }
-    })
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+
+        newUser.save(function(err){
+            if(!err){
+                res.render("secrets");
+            }else{
+                res.status(500).send("Error while registering /n" + err);
+            }
+        });
+    });
 });
 
 app.route("/login")
@@ -60,13 +66,35 @@ app.route("/login")
 })
 
 .post(function(req, res){
-    User.findOne({$and: [{email: req.body.username}, {password: md5(req.body.password)}]}, function (err, foundUser){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // User.findOne({$and: [{email: req.body.username}, {password: md5(req.body.password)}]}, function (err, foundUser){
+    //     if(!err){
+    //         res.render("secrets");
+    //     }else{
+    //         res.status(401).send("Not Authorized!");
+    //     }
+    // });
+
+    User.findOne({email: username}, function(err, foundUser){
         if(!err){
-            res.render("secrets");
+            if(foundUser){
+                bcrypt.compare(password, foundUser.password, function (err, result){
+                    if(result === true){
+                        res.render("secrets");
+                    }else{
+                        res.status(401).send("Unauthorized!");
+                    }
+                });
+            }else{
+                res.status(401).send("Unauthorized!");
+            }
         }else{
-            res.status(401).send("Not Authorized!");
+            res.status(401).send("Unauthorized!");
         }
     });
+
 });
 
 
