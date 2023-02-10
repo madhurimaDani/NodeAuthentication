69df4e4a75b6a50806dbba44a584adab42f4ec42
madhurimaDani <madhurimadani@gmail.com>
2023-02-10 12:33:08 -0800

    styled social login button, added submit routes, updated secrets page

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ mongoose.set('strictQuery', true);
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -102,11 +103,15 @@ app.route("/register")
 });
 
 app.get("/secrets", function(req, res){
-    if(req.isAuthenticated()){
-        res.render("secrets");
-    }else{
-        res.redirect("/login");
-    }
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUsers){
+                res.render("secrets", {usersWithSecrets: foundUsers});
+            }
+        }
+    });
 });
 
 app.route("/login")
@@ -134,6 +139,34 @@ app.route("/login")
         }
     });
 
+});
+
+
+app.route("/submit")
+
+.get(function(req, res){
+    if(req.isAuthenticated()){
+        res.render("submit");
+    }else{
+        res.redirect("/login");
+    }
+})
+
+.post(function(req, res){
+   const submittedSecret = req.body.secret;
+   console.log(req.user.id);
+   User.findById(req.user.id, function(err, foundUser){
+       if(err){
+           console.log(err);
+       }else{
+           if(foundUser){
+               foundUser.secret = submittedSecret;
+               foundUser.save(function(){
+                   res.redirect("/secrets");
+               });
+           }
+       }
+   });
 });
 
 
